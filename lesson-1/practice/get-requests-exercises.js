8eb73e01c2bbd28a7bc507fc4b26ff48dbeec3e2
Keyairra Wright <keyairra.wright@gmail.com>
2020-02-02 13:51:48 -0500

    add lesson one exercises and solutions

---
+++ lesson-1/practice/get-requests-exercises.js
@@ -0,0 +1,92 @@
+/**
+ * For exercises 1-4, use the `Random User Generator API` and
+ * write your solutions using .then() syntax.
+ *
+ * TIPS:
+ * 1) Read the documentation for the  `Random user API`:
+ * https://randomuser.me/documentation.
+ *
+ * 2) Drag the `get-requests.html` file to a Google Chrome browser tab.
+ * Open up the developer console.
+ *
+ * 3) Keep your browsers and console tiddy by commenting out all code
+ * except for the solution to the exercise that you are currently
+ * working on.
+ *
+ * 4) Code out your solution underneath each exercise.
+ */
+
+/**
+ * Exercise 1:
+ *
+ * Create a function called `logUsers` to log all information for
+ * 500 users. Ensure that you only log the `results` from the
+ * response object.
+ */
+
+/**
+ * Exercise 2:
+ *
+ * Create a function called `listTenNames` and then invoke it.
+ * This function needs to
+ * 1. log the first names of 10 random users.
+ * 2. number each of the names from 1-10.
+ */
+
+/**
+ * Exercise 3:
+ * Create a function called `createPhoneBook` and invoke it.
+ * This function needs to:
+ * 1. get phone numbers for 25 users.
+ * 2. and print each users name next to their phone number in a list
+ * that shows up in the browser.
+ * 3. *optional*: also print the names and phone numbers to the console.
+ *
+ */
+
+/**
+ * Exercise 4:
+ *
+ * Create a function called `createPhotoAlbum` and invoke it.
+ *
+ * This function needs to:
+ * 1) get 10 users who all identify as the same gender and same nationality
+ * 2) display a simple html table with the users' names and photos
+ * 3) give the table a title and ensure both rows have a label.
+ *
+ * You may choose the size of the photo to display. Styling the table is optional.
+ *
+ * NOTE: The random user api allows for parameters to be combined
+ * using this format: `randomuser.me/api/?parameter1=value1&parameter2=value2`.
+ * See the `Pagination` portion of the documentation to see an example query
+ * with multiple parameters
+ */
+
+/**
+ * For exercises 5-6, use the `Joke API` and
+ * write your solutions using the async/await syntax
+ *
+ * TIPS:
+ * 1) Read the documentation for the  `Joke API`:
+ * https://sv443.net/jokeapi/v2#endpoints.
+ *
+ */
+
+/**
+ * Exercise 5:
+ * Create a function called `logAJoke`.
+ *
+ * This function needs to:
+ * 1) get 1 joke that is about programming and is in a two-part format.
+ * 2) log the setup and delivery of the joke on separate lines
+ */
+
+/**
+ * Exercise 6:
+ * Create a function called `showApiInfo`.
+ *
+ * This function needs to:
+ * 1) use the /info endpoint to get information on about the joke API
+ * 2)  within the body of the browser, show the info portion of the response object
+ *
+ */
